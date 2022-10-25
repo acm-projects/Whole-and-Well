d29b760cb748e68f8ad5911513465f4f7a2fb106
sherwin-valan <sherwin.valan@gmail.com>
2022-10-25 01:22:36 -0500

    test 2

--- signup/lib/main.dart
+++ signup/lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async  {
-  WidgetsFlutterBinding.ensureInitialized(); // whta gtgoos]
+  WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(); // hola
 
   runApp(const MyApp());
 }
